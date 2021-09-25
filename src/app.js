d5908e3b703dfd27b4d0a9ef217a568a08c0f297
Silvia <silviatmnez@gmail.com>
2021-09-25 12:02:05 +0000

    excuse generator created and styles applied

--- src/app.js
+++ src/app.js
@@ -7,5 +7,33 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuse = generateExcuse();
+  document.querySelector("#excuse").innerHTML = excuse;
 };
+
+function generateExcuse() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  return (
+    randElement(who) +
+    " " +
+    randElement(action) +
+    " " +
+    randElement(what) +
+    " " +
+    randElement(when)
+  );
+}
+
+function randElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
